e3c01d4aadee0f7df92463adef8c8124eae332b2
unknown <mishoxuco2001@gmail.com>
2021-05-31 10:33:51 +0400

    com

--- fetch.js
+++ fetch.js
@@ -64,20 +64,19 @@ product.addEventListener('change',function(e){
     lay(3,4,6,15)
   }
 });
-const prodmax=document.querySelector(".maxproduct");
 product.addEventListener('change',function(e){
   if(product.value=="3"){
     lay(3,4,6,20)
   }
 });
-
 function lay(col3=3,col4=4,col6=6,qual=20){
 let tmp='';
   for(let i=0;i<qual;i++){
     tmp+=`
+    
     <div class="col-lg-${col3} col-md-${col4} col-sm-${col6} mb-2">
 <div class="card rem" style="height: 100%;">
-<img src="${data[i].image}" class="card-img-top" "alt="..."> 
+<img src="${data[i].image}" class="card-img-top"style="height: 100%;" "alt="..."> 
 <div class="card-body">
 <i class="bi bi-star-fill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
@@ -101,7 +100,6 @@ let tmp='';
 </div>`
 
 }
-
   mainly.innerHTML=tmp;
   console.log(qual);
 }
